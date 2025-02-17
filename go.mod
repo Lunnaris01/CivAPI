57d0c4d9426e755d731b6e8dd0ec777bcec525cc
Lunnaris01 <Julian_Ullrich@live.de>
2025-02-17 04:44:07 +0100

    basic env setup and apicfg

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/Lunnaris01/Civ_API
 
 go 1.23.5
+
+require github.com/joho/godotenv v1.5.1 // indirect
--- main.go
+++ main.go
@@ -3,8 +3,35 @@ package main
 
 import(
 	"fmt"
+	"os"
+	"github.com/joho/godotenv"
+
 )
 
+
+
+type apiConfig struct {
+	//dbQueries *database.Queries
+	dbURL string
+	dbToken string
+	platform string
+}
+
 func main(){
-	fmt.Println("Hello World")
+	fmt.Println("Civ API started!")
+	godotenv.Load()
+
+	env_platform := os.Getenv("PLATFORM")
+	env_dbURL := os.Getenv("TURSO_DATABASE_URL")
+	env_dbToken := os.Getenv("TURSO_AUTH_TOKEN")
+
+
+	apiCfg := apiConfig {
+		dbURL: env_dbURL,
+		dbToken: env_dbToken,
+		platform: env_platform,
+	}
+
+	fmt.Println(apiCfg)
+
 }
